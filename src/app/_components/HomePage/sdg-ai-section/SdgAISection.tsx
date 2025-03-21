1f1dc89feb138b23b8fbcb4303a426ea7b3e6575
azzumar247 <azzumar.tbh@gmail.com>
2025-03-22 06:13:15 +0800

    comment out the theme mode code

--- src/app/_components/HomePage/sdg-ai-section/SdgAISection.tsx
+++ src/app/_components/HomePage/sdg-ai-section/SdgAISection.tsx
@@ -1,25 +1,25 @@
 "use client";
 
 import { GlobeAltIcon } from "@heroicons/react/24/solid";
-import { useTheme } from "next-themes";
+// import { useTheme } from "next-themes";
 import Image from "next/image";
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 
 const SdgAISection = () => {
-    const { theme, resolvedTheme } = useTheme();
-    const [mounted, setMounted] = useState(false);
+    // const { theme, resolvedTheme } = useTheme();
+    // const [mounted, setMounted] = useState(false);
 
 
-    useEffect(() => {
-        setMounted(true);
-    }, []);
+    // useEffect(() => {
+    //     setMounted(true);
+    // }, []);
 
-    if (!mounted) return null;
+    // if (!mounted) return null;
 
-    const pattern =
-        (theme || resolvedTheme) === "dark"
-            ? "/images/homepage/sdgAI/Pattern-BG-dark.svg"
-            : "/images/homepage/sdgAI/Pattern-BG.svg";
+    // const pattern =
+    //     (theme || resolvedTheme) === "dark"
+    //         ? "/images/homepage/sdgAI/Pattern-BG-dark.svg"
+    //         : "/images/homepage/sdgAI/Pattern-BG.svg";
 
     const aiTools = [
         {
